71c7a40946fd014b702af59c03cbdb90b182de13
Alfredo Marranjal <Alfredo Marranjal@LAPTOP-C8OKF3S2>
2025-03-31 18:08:45 +0100

    Primeiro commit do projeto

---
+++ AmigoOcultoAPP/src/main/java/com/viacerta/amigoocultoapp/AmigoOcultoAPP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.viacerta.amigoocultoapp;
+
+/**
+ *
+ * @author Alfredo Marranjal
+ */
+public class AmigoOcultoAPP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
